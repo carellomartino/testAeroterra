8d329ed0051b84690087631f99c2d57ee61bdf98
Nicolas <carellomartino@gmail.com>
2019-09-26 20:59:56 -0300

    last commit

--- script2.js
+++ script2.js
@@ -1,6 +1,36 @@
+let latitud = ''
+let longitud = ''
+
+
 function submitt(){
-    let x = document.getElementById("form-id")
-    for (let i = 0; i < x.length ;i++) {
-        console.log(x.elements[i].value)
+    console.log("entre")
+    let y = document.getElementById("form-id")
+    latitud = y.elements[3].value
+    longitud = y.elements[4].value
+    console.log('JOJOJOJOJ', latitud, longitud)
+    for (let i = 0; i < y.length; i++) {
+        console.log(y.elements[i].value)
+        // if(i == 3){
+        //     latitud = y.elements[i].value
+        //     console.log("LALALALALLA", latitud)
+        // } else if(i == 4){
+        //     longitud = y.elements[i].value
+        // }
       }
-}
+    }
+    
+
+
+
+function initMap() {
+    console.log("lati", latitud)
+    console.log("long", longitud)
+    // var latitud = 
+    
+    var location = {lat: -25.344, lng: 131.036};
+    
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 14, center: location});
+    
+    var marker = new google.maps.Marker({position: location, map: map});
+  }
